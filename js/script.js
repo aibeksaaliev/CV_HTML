b5ab1b6c16c1c9fd0862b78f2d3407f2eb064abc
Aibek_Saaliev <dokaidea@gmail.com>
2023-07-12 01:55:26 +0600

    Updated #2

--- js/script.js
+++ js/script.js
@@ -29,9 +29,87 @@ document.addEventListener('DOMContentLoaded',function(event){
     StartTextAnimation(0);
 });
 
-const circularProgress = document.querySelectorAll(".circular-progress");
+// const circularProgress = document.querySelectorAll(".circular-progress");
+//
+// Array.from(circularProgress).forEach((progressBar) => {
+//     const progressValue = progressBar.querySelector(".circular-progress__percentage");
+//     const innerCircle = progressBar.querySelector(".circular-progress__inner-circle");
+//     let startValue = 0,
+//         endValue = Number(progressBar.getAttribute("data-percentage")),
+//         speed = 50,
+//         progressColor = progressBar.getAttribute("data-progress-color");
+//
+//     const progress = setInterval(() => {
+//         startValue++;
+//         progressValue.textContent = `${startValue}%`;
+//         progressValue.style.color = `${progressColor}`;
+//
+//         innerCircle.style.backgroundColor = `${progressBar.getAttribute(
+//             "data-inner-circle-color"
+//         )}`;
+//
+//         progressBar.style.background = `conic-gradient(${progressColor} ${
+//             startValue * 3.6
+//         }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
+//         if (startValue === endValue) {
+//             clearInterval(progress);
+//         }
+//     }, speed);
+// });
 
-Array.from(circularProgress).forEach((progressBar) => {
+const circularProgressElements = document.querySelectorAll(".circular-progress");
+const options = {
+    root: null, // Используем видимую область браузера как корневой элемент
+    rootMargin: "0px",
+    threshold: 0.5, // Порог видимости элемента
+};
+
+function runCircularProgressOnIntersection(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const progressBar = entry.target;
+            const progressValue = progressBar.querySelector(".circular-progress__percentage");
+            const innerCircle = progressBar.querySelector(".circular-progress__inner-circle");
+            let startValue = 0,
+                endValue = Number(progressBar.getAttribute("data-percentage")),
+                speed = 50,
+                progressColor = progressBar.getAttribute("data-progress-color");
+
+            const progress = setInterval(() => {
+                startValue++;
+                progressValue.textContent = `${startValue}%`;
+                progressValue.style.color = `${progressColor}`;
+
+                innerCircle.style.backgroundColor = `${progressBar.getAttribute(
+                    "data-inner-circle-color"
+                )}`;
+
+                progressBar.style.background = `conic-gradient(${progressColor} ${
+                    startValue * 3.6
+                }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
+                if (startValue === endValue) {
+                    clearInterval(progress);
+                    observer.unobserve(progressBar); // Прекращаем отслеживать элемент после запуска анимации
+                }
+            }, speed);
+        }
+    });
+}
+
+// Создаем экземпляр Intersection Observer
+const observer = new IntersectionObserver(runCircularProgressOnIntersection, options);
+
+// Начинаем отслеживать каждый элемент
+circularProgressElements.forEach((progressBar) => {
+    observer.observe(progressBar);
+});
+
+
+
+
+
+function runCircularProgress(event) {
+    const progressBar = event.target.closest(".circular-progress");
     const progressValue = progressBar.querySelector(".circular-progress__percentage");
     const innerCircle = progressBar.querySelector(".circular-progress__inner-circle");
     let startValue = 0,
@@ -55,4 +133,30 @@ Array.from(circularProgress).forEach((progressBar) => {
             clearInterval(progress);
         }
     }, speed);
-});
+
+    progressBar.addEventListener("mouseleave", () => {
+        clearInterval(progress);
+        startValue = 0;
+        progressValue.textContent = `${endValue.toString()}%`;
+        progressValue.style.color = progressColor;
+        innerCircle.style.backgroundColor = "#222222";
+        progressBar.style.background = `conic-gradient(${progressColor} ${
+            endValue * 3.6
+        }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
+    });
+}
+
+function downloadCV() {
+    const fileUrl = 'pdf/Software Engineer Resume in Red Black Simple Style.pdf';
+
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'CV Frontend Aibek Saaliev.pdf';
+    link.target = '_blank';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+
